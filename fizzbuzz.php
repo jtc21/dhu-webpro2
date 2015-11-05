75e408a6e7fb9617965a890adcd71d70bbbf1af9
jtc21 <info@jtc21.com>
2015-11-05 09:42:49 +0900

    Gitを追加

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,11 +1,14 @@
 <?php
 	for($setNum = 1 ; $setNum <= 100 ; $setNum ++){
-		if($setNum % 3 == 0 || $setNum % 5 == 0){
+		if($setNum % 3 == 0 || $setNum % 5 == 0 || $setNum % 7 == 0){
 			if($setNum % 3 == 0){
 				echo "Fizz!";
 			}
 			if($setNum % 5 == 0){
 				echo "Buzz!";
+			}
+			if($setNum % 7 == 0){
+				echo "Git!";
 			}
 			echo "<br />\n";
 		} else {
