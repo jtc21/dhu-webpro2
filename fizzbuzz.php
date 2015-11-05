362bf3735f657fe27032ce2e9624af6218ad6687
jtc21 <info@jtc21.com>
2015-11-05 09:41:32 +0900

    FizzとBuzzを追加

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,6 +1,16 @@
 <?php
 	for($setNum = 1 ; $setNum <= 100 ; $setNum ++){
-		echo $setNum."<br />\n";
+		if($setNum % 3 == 0 || $setNum % 5 == 0){
+			if($setNum % 3 == 0){
+				echo "Fizz!";
+			}
+			if($setNum % 5 == 0){
+				echo "Buzz!";
+			}
+			echo "<br />\n";
+		} else {
+			echo $setNum."<br />\n";
+		}
 	}
 	
 	
